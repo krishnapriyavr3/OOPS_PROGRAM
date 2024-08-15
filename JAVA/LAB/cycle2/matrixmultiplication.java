31a3e105c17ce3138335aecb9fa277e0d66031d2
krishnapriyavr3 <krishnapriyavr3@gmail.com>
2024-08-15 11:35:08 +0530

    Add files via upload

---
+++ JAVA/LAB/cycle2/matrixmultiplication.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+class MatixMultiplication
+{
+    public static void main(String args[])
+    {
+        int n;
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the no of row and coloumns");
+        n = s.nextInt();
+        int[][] a = new int[n][n];
+        int[][] b = new int[n][n];
+        int[][] c = new int[n][n];
+        System.out.println("Enter the elements of 1st martix  \n");
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                a[i][j] = s.nextInt();
+            }
+        }
+        System.out.println("Enter the elements of 2nd martix \n");
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                b[i][j] = s.nextInt();
+            }
+        }
+        System.out.println("Multiplying the matrices...");
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                for (int k = 0; k < n; k++)
+                {
+                    c[i][j] = c[i][j] + a[i][k] * b[k][j];
+                }
+            }
+        }
+        System.out.println("The product is:");
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                System.out.print(c[i][j] + " ");
+            }
+            System.out.println();
+        }
+        s.close();
+    }
+}
