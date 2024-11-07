6934e31321c75effe7280740f3c37d41cfaff756
krishnapriyavr3 <krishnapriyavr3@gmail.com>
2024-11-07 11:18:53 +0530

    Update file.java

--- JAVA/LAB/cycle 4/file.java
+++ JAVA/LAB/cycle 4/file.java
@@ -1,28 +1,36 @@
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-class file {
-	public static void main(String[] args)
-	{
-		try {
-			FileReader fr = new FileReader("gfgInput.txt");
-			FileWriter fw = new FileWriter("gfgOutput.txt");
-			String str = "";
-			int i;
-			while ((i = fr.read()) != -1) {
-				str += (char)i;
-			}
-			System.out.println(str);
-			fw.write(str);
-			fr.close();
-			fw.close();
-			System.out.println(
-				"File reading and writing both done");
-		}
-		catch (IOException e) {
-			System.out.println(
-				"There are some IOException");
-		}
-	}
-}
 
+import java.io.*;
+class filehandling
+{
+	public static void main(String args[])
+	{
+		try
+		{
+			FileReader reader=new FileReader("input.txt");
+			FileWriter writer=new FileWriter("output.txt");
+			int character;
+			System.out.println("reading from the file and writing to the file");
+			while((character=reader.read())!=-1)
+			{
+				writer.write(character);
+				System.out.print((char) character);
+			}
+			System.out.println();
+			reader.close();
+			writer.close();
+			System.out.println("\nFile content written successfully to output file.");
+		}
+		catch(FileNotFoundException e)
+		{
+		System.out.println("filenot found exception occurred");
+		}
+		catch(IOException e)
+		{
+			System.out.println("IO  exception occurred");
+		}
+		catch(Exception e)
+		{
+			System.out.println("error occured");
+		}
+}
+}
