191792f3a83fc85246ed5e80d0b774ee5f1d6b55
krishnapriyavr3 <krishnapriyavr3@gmail.com>
2024-11-07 11:21:46 +0530

    Update file1.java

--- JAVA/LAB/cycle 4/file1.java
+++ JAVA/LAB/cycle 4/file1.java
@@ -1,16 +1,38 @@
-import java.io.FileWriter;
-import java.io.IOException;
-class CreateFile
+
+import java.io.*;
+class filehandling
 {
 	public static void main(String args[])
-   	{
-		String str = "File Handling in java using"+"FileWriter"; 
-		FileWriter fw=new FileWriter("output.txt");
-		for (int i = 0; i < str.length(); i++) 
-			fw.write(str.charAt(i)); 
-
-		System.out.println("Writing successful");
-		fw.close(); 
-	} 
+	{
+		String content="this is test data";
+		try{
+				FileWriter writer=new FileWriter("test.txt");
+				writer.write(content);
+				System.out.println("content written to file");
+				writer.close();
+				}
+		catch(Exception e)
+		{
+				System.out.println("Error occured");
+		}
+		try
+		{
+				FileReader reader=new FileReader("test.txt");
+				int data;
+				while((data=reader.read())!=-1)
+				{
+					System.out.print((char) data);
+				}
+				System.out.println();
+				reader.close();
+}
+		catch(Exception e)
+		{
+				System.out.println("error occured");
+		}
+	}
 }
 
+
+				
+	
